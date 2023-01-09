6242363561433e6e1be62c2da6668a05a5c10cd6
EC2 Default User <ec2-user@ip-172-31-47-105.ap-northeast-1.compute.internal>
2023-01-09 07:57:09 +0000

    検索機能 Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   get 'relationships/followings'
   get 'relationships/followers'
-   get "home/about"=>"homes#about"
+  get "home/about"=>"homes#about"
   root to: "homes#top"
- get 'users/new'
- devise_for :users
+  get 'users/new'
+  get "search" => "searches#search"
+  devise_for :users
   
  
   
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resource :favorites, only: [:create, :destroy]
-   resources :post_comments, only: [:create,:destroy]  
+  resources :post_comments, only: [:create,:destroy]  
   end
   resources :users, only: [:index,:show,:edit,:update]
   
